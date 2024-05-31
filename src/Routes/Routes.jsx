e50726cf725e8d7fd50774d87bfd4065f3f7905c
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-31 14:18:09 +0600

    added the basic navbar for the website

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Main/Main";
-import Home from "../src/pages/Home/Home/Home";
+import Home from "../pages/Home/Home/Home";
 
 export const router = createBrowserRouter([
     {
