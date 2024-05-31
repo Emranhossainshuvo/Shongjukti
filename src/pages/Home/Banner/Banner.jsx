9072df98b0b32460b67da90584fe26fa232a0f92
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-31 16:13:20 +0600

    added carousel in the banner

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -11,31 +11,112 @@ const Banner = () => {
         <>
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
                 <SwiperSlide>
-                    Slide 1
+                    <div className="hero min-h-[80vh]" style={{ backgroundImage: 'url(https://i.ibb.co/47Rqnx9/joshua-earle-9idq-IGr-Lu-TE-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    Slide 2
+                    <div className="hero min-h-[80vh]" style={{ backgroundImage: 'url(https://i.ibb.co/47Rqnx9/joshua-earle-9idq-IGr-Lu-TE-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    Slide 3
+                    <div className="hero min-h-[80vh]" style={{ backgroundImage: 'url(https://i.ibb.co/47Rqnx9/joshua-earle-9idq-IGr-Lu-TE-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    Slide 4
+                    <div className="hero min-h-[80vh]" style={{ backgroundImage: 'url(https://i.ibb.co/47Rqnx9/joshua-earle-9idq-IGr-Lu-TE-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    Slide 5
+                <div className="hero min-h-[80vh]" style={{ backgroundImage: 'url(https://i.ibb.co/47Rqnx9/joshua-earle-9idq-IGr-Lu-TE-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    Slide 6
+                    <div className="hero min-h-[80vh]" style={{ backgroundImage: 'url(https://i.ibb.co/47Rqnx9/joshua-earle-9idq-IGr-Lu-TE-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    Slide 7
+                    <div className="hero min-h-[80vh]" style={{ backgroundImage: 'url(https://i.ibb.co/47Rqnx9/joshua-earle-9idq-IGr-Lu-TE-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    Slide 8
+                    <div className="hero min-h-[80vh]" style={{ backgroundImage: 'url(https://i.ibb.co/47Rqnx9/joshua-earle-9idq-IGr-Lu-TE-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    Slide 9
+                    <div className="hero min-h-[80vh]" style={{ backgroundImage: 'url(https://i.ibb.co/47Rqnx9/joshua-earle-9idq-IGr-Lu-TE-unsplash.jpg)' }}>
+                        <div className="hero-overlay bg-opacity-60"></div>
+                        <div className="hero-content text-center text-neutral-content">
+                            <div className="max-w-md">
+                                <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                                <button className="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
                 </SwiperSlide>
             </Swiper>
         </>
