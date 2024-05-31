576d314996db43b9a4a3016427d09986667518ae
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-31 16:08:12 +0600

    added daisyui for useing predesign components

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -10,15 +10,33 @@ const Banner = () => {
     return (
         <>
             <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
+                <SwiperSlide>
+                    Slide 1
+                </SwiperSlide>
+                <SwiperSlide>
+                    Slide 2
+                </SwiperSlide>
+                <SwiperSlide>
+                    Slide 3
+                </SwiperSlide>
+                <SwiperSlide>
+                    Slide 4
+                </SwiperSlide>
+                <SwiperSlide>
+                    Slide 5
+                </SwiperSlide>
+                <SwiperSlide>
+                    Slide 6
+                </SwiperSlide>
+                <SwiperSlide>
+                    Slide 7
+                </SwiperSlide>
+                <SwiperSlide>
+                    Slide 8
+                </SwiperSlide>
+                <SwiperSlide>
+                    Slide 9
+                </SwiperSlide>
             </Swiper>
         </>
     );
