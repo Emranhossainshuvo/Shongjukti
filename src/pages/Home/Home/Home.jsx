993326d240457f724a411f69d9b2b6b550335005
EmranHossain <emran.shuvo.dev@gmail.com>
2024-05-31 16:18:28 +0600

    basic blog card added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
+import Blogs from "../Blogs/Blogs";
 
 const Home = () => {
     return (
         <>
             <h3>Hello, this is home</h3>
             <Banner />
+            <Blogs />
         </>
     );
 };
