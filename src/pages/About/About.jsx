d99bc919c3966d342dc878a881ad179ab4125ca0
EmranHossain <emran.shuvo.dev@gmail.com>
2024-06-01 09:07:13 +0600

    about page careated and banner added and some text and images added

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,58 @@
+
+const About = () => {
+    return (
+        <>
+            <div>
+                <div className="my-10">
+                    <img src="https://cdn.shopify.com/s/files/1/0070/7032/files/how-to-write-an-about-us-wip1.png?v=1618511825" alt="" />
+                </div>
+
+                <div>
+                    <div className="flex flex-col sm:flex-row md:flex-row px-[2%] sm:px-[5%] lg:px-[8%] mt-28 gap-10">
+                        <img
+                            src={image2}
+                            alt=""
+                            className="w-[410px] md:w-[300px] rounded-md object-cover border-t-8 border-l-8 border-sky-900 transition delay-150 duration-300 ease-in-out hover:scale-90"
+                        />
+                        <div>
+                            <h2 className="text-xl md:text-2xl lg:text-4xl font-bold  text-sky-800">
+                                Join with best fitness coach
+                            </h2>
+                            <p className="max-w-xl py-7 text-sm">
+                                Our personal trainers can help you meet your fitness goals. They can
+                                become your teacher, your motivator, your coach and your friend. Our
+                                personal trainers are degreed and certified by an accredited fitness
+                                organization.
+                            </p>
+                            <ul className="list-image text-sm">
+                                <li className="flex gap-4">
+                                    <IoCheckmarkDone className="w-[20px] text-sky-700 " />
+                                    Teach you to exercise using proper form to prevent injury
+                                </li>
+                                <li className="flex gap-4 py-3">
+                                    <IoCheckmarkDone className="w-[20px] text-sky-700 " />
+                                    Add diversity to your workout to get over a weight-loss plateau
+                                </li>
+                                <li className="flex gap-4">
+                                    <IoCheckmarkDone className="w-[20px] text-sky-700  " />
+                                    Boost athletic performance with sport-specific training
+                                </li>
+                                <li className="flex gap-4 py-3">
+                                    <IoCheckmarkDone className="w-[20px] text-sky-700  " />
+                                    Give you the accountability and motivation to get to the gym
+                                </li>
+                                <li className="flex gap-4">
+                                    <IoCheckmarkDone className="w-[20px] text-sky-700 " />
+                                    Encourage you to try new fitness challenges and drive your health
+                                    forward
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default About;
