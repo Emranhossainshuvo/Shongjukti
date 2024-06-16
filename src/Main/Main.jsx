f6b750dcc80c744af1372fc6f61d482f0fdc4fab
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-16 07:56:57 +0600

    added simple footer to the website

--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
+import Footer from "../Components/Shared/Footer/Footer";
 
 const Main = () => {
     return (
         <div className="max-w-screen-xl mx-auto">
             <Navbar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
