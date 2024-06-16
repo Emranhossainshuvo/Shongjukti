7dfdd86988c6f8b80b811f6524dad50e1a5d60d6
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-06-16 10:00:10 +0600

    added login functionality with google firebase

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,26 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../Providers/AuthProvider";
 
 const Login = () => {
+
+    const {logInUser} = useContext(AuthContext); 
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); 
+        const form = e.target; 
+        const email = form.email.value;
+        const password = form.password.value;
+
+        logInUser(email, password)
+        .then(result => {
+            const user = result.user;
+            console.log(user)
+        })
+
+    }
+
+
 
     return (
         <section className="h-[100vh] flex items-center justify-center bg-[#31473a]">
@@ -8,9 +28,9 @@ const Login = () => {
             <section className="bg-[#f1f1f2] w-[24%] rounded-xl">
                 <h3 className="text-center text-3xl font-semibold mb-2 mt-3">Talks chat</h3>
                 <p className="text-center mb-4 text-xl font-semibold">Login</p>
-                <form className="flex flex-col gap-7 px-10">
-                    <input placeholder="Email" className="h-10 outline-none ps-3 rounded-md" type="email" name="" id="" />
-                    <input placeholder="Password" className="h-10 outline-none ps-3 rounded-md" type="password" name="" id="" />
+                <form onSubmit={handleSubmit} className="flex flex-col gap-7 px-10">
+                    <input placeholder="Email" className="h-10 outline-none ps-3 rounded-md" type="email" name="email" id="" />
+                    <input placeholder="Password" className="h-10 outline-none ps-3 rounded-md" type="password" name="password" id="" />
 
                     <input className="h-10 cursor-pointer bg-[#31473a] text-white text-lg font-semibold rounded-md" type="submit" value="Login" />
                     
